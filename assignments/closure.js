3fe89a5d7770d74be83074f9f29068da6c74c775
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-05-07 15:28:45 -0400

    WIP, finished callback and closure MVP

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const name = 'Tyler';
+function whatIsMyName () {
+  return name;
+}
+console.log(whatIsMyName());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
